0e5d7ee9c05d7001ddc6ba8452b5e124b11c71ab
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-10-11 14:22:11 -0400

    Created DOM variable and JS variables.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+// console.log("Hello World");
+// DOM VARIABLES
+var inputEl = document.getElementById("inputCity");
+
+// JS VARIABLES
+var citiesDisplay = [
+  "Austin",
+  "Chicago",
+  "New York",
+  "Orlando",
+  "San Francisco",
+  "Seattle",
+  "Denver",
+  "Atlanta",
+];
+for (var i = 0; i < citiesDisplay.length; i++) console.log(citiesDisplay);
+
+// WHEN I search for a city
+
+// I am presented with current and future conditions for that city and that city is added to the search history
