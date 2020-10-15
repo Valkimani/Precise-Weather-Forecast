8d32cfe6b4868d04a4310d5e8f4964b8cf5c435b
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-10-14 21:47:25 -0400

    Created a fo loop for the search history and added the Ajax method.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // console.log("Hello World");
-// DOM VARIABLES
-var inputEl = document.getElementById("inputCity");
-var submitSearchEl = document.querySelector(".btn btn-primary");
 
+var fiveDayURL =
+  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=5013ea23d7c0a9940f6e40866a7d4524&units=imperial";
+
+// DOM VARIABLES
+var inputEl = $("#inputCity");
+var submitSearchEl = $("#btn");
+var userSearch = $("displayCity");
 // JS VARIABLES
 var citiesDisplay = [
   "Austin",
@@ -15,21 +19,37 @@ var citiesDisplay = [
   "Atlanta",
 ];
 
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {});
-// / WHEN I search for a city
-// search for a city
-var userSearch = document.querySelector("inputCity");
-
+/// WHEN I search for a city
 for (var i = 0; i < citiesDisplay.length; i++) {}
-function weatherSearch(city) {}
-// saving users input to a variable
+function weatherSearch(city) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=5013ea23d7c0a9940f6e40866a7d4524&units=imperial";
 
-if (userSearch === searchOuPut) {
-  var searchCity = inputEl.val();
+  // Moved inside the function to pull a city
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    console.log(response.main.temp);
+    //***** */ I am presented with current and future conditions for that city and that city is added to the search history
+    pTag = $("<p>").text(city + response);
+    $("#outPut").append(pTag);
+  });
 }
+
+submitSearchEl.on("click", function (event) {
+  event.preventDefault();
+  console.log("this is working!");
+  // call weather search function
+  var searchCity = inputEl.val();
+  // calling weathersearch and parsing a variable
+  weatherSearch(searchCity);
+});
+
+// saving users input to a variable
 
 // Function definitions
 // Adding a function that displays the search results
@@ -62,4 +82,4 @@ if (userSearch === searchOuPut) {
 // THEN I am presented with the last searched city forecast
 
 // Add an event listener
-submitSearchEl.addEventListener("click", userSearch);
+// submitSearchEl.addEventListener("click", userSearch);
